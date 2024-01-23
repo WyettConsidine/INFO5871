9e35c5b0c5668985f436e84e9d885df056202413
Wyett Considine <43249892+WyettConsidine@users.noreply.github.com>
2024-01-23 11:15:46 -0600

    Create arXivAPI.py

---
+++ scripts/arXivAPI.py
@@ -0,0 +1,48 @@
+import pandas as pd 
+import requests
+import pandas_read_xml as pdx
+import xml.etree.ElementTree as ET
+import re
+
+def basicArXivCall(subject, max_results=10):
+    #path = "C:/Users/wyett/OneDrive/Documents/INFO5871/"
+    endpoint = "http://export.arxiv.org/api/query?"
+    url = endpoint+"search_query=all:"+subject+f"&sortBy=submittedDate&sortOrder=descending&max_results={max_results+1}"
+    try:
+        requests.get(url)
+    except requests.ConnectionError as e:
+        print(e)
+    else:
+        print('API status: 200')
+        with requests.get(url) as response:             
+
+            root = ET.fromstring(response.content)
+            entries = []
+            dictionary = {}
+            
+            for child in root.iter('*'):
+                tag = re.sub("\{.*?\}","",child.tag)
+                if tag == 'entry':
+                    entries.append(dictionary)
+                    dictionary = {}
+                if tag in ['id','updated','published','title','summary','primary_category']:
+                    dictionary[tag] = child.text
+                    
+            df = pd.DataFrame(entries)
+
+            df['summary'] = df['summary'].apply(lambda x: str(x).replace('\n',' '))
+            df['summary'] = df['summary'].apply(lambda x: str(x).replace('\t',' '))
+            print(df.columns)
+            print(df.info)
+
+            #df.to_csv(path+'/arXiv_AI.txt',sep = '\t',index = False)
+
+            #df = pd.read_csv(path+'/arXiv_AI.txt',sep = '\t')        
+
+
+def main():
+    basicArXivCall('nulcear')
+
+
+if __name__ == "__main__":
+    main()
