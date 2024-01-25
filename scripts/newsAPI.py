d00d0b91283f72b99b1ad8f9a9e03849a9638b51
Wyett Considine <43249892+WyettConsidine@users.noreply.github.com>
2024-01-25 15:14:24 -0600

    news api, get soruces
    get list of applicable news api article sources form the us, then dictionaried by the political lean.

--- scripts/newsAPI.py
+++ scripts/newsAPI.py
@@ -2,14 +2,54 @@ import requests  #to query the API
 import re  #regular expressions
 import pandas as pd   # for dataframes
 
-from datetime import date
-from dateutil.relativedelta import relativedelta
+import csv
 
 #Addapted from https://newsapi.org/docs/client-libraries/python
 
+#List of sources and their polititcal lean:
+#goal: approximate normal curve between sources of politically Left, left learning, center, right leaning, and right
+
+
 # Init
 api_key='efa4bfad3e8749d19ffd8298771cbfdb'
-endpointAr='https://newsapi.org/v1/articles'
+endpointArticles='https://newsapi.org/v1/articles'
+endpointSources = 'https://newsapi.org/v2/top-headlines/sources'
+
+
+#political lean of soruces determined with https://www.allsides.com/media-bias/ and https://mediabiasfactcheck.com/
+def getSources(endpoint=endpointSources, key=api_key, country = 'us'):
+    URLPost = {'apiKey': key, 
+                    #'category':'general',
+                    'language': 'en',
+                    'country':country,
+                    }    
+    respRaw = requests.get(endpoint,URLPost)
+    resp = respRaw.json()
+    print(resp['status'])
+    if resp['status'] != 'ok':
+        print("Non-Ok Status error from NewsApi. Status = " + resp['status'])
+    else:
+        sources = []
+        for src in resp['sources']:
+            if src['category'] in ('general', 'science', 'technology', 'health'):
+                #print(src['name'] + ", " + src['category'])
+                sources.append((src['name'] , src['category']))
+        return sources
+
+def getSourceBiasLists():   
+    with open('C:/Users/wyett/OneDrive/Documents/INFO5871/resourceFiles/newsAPISourceLists.csv', newline='') as f:
+        reader = csv.reader(f)
+        sourceFile = list(reader)
+        f.close()
+    sources = []
+    lean = ['left', 'leftLean', 'center', 'rightLean', 'right']
+    for srclist in sourceFile:
+        sources.append(srclist[1:])
+    newsSourceDict = dict(zip(lean, sources))
+    print(newsSourceDict)
+    return newsSourceDict
+        
+
 
 def basicNewsAPICall(endpoint, key, subject, source):
     URLPost = {'apiKey': key,
@@ -19,7 +59,14 @@ def basicNewsAPICall(endpoint, key, subject, source):
                     'totalRequests': 75,
                     'q':subject}
     resp1 = requests.get(endpoint,URLPost)
-    print(resp1.json()['articles'][0]['authors'])
+    print(resp1.json()['articles'])
 
 
-basicNewsAPICall(endpointAr,api_key,'Nuclear Energy', 'bbc-news')
+
+
+
+#basicNewsAPICall(endpointArticles,api_key,'Nuclear Energy', 'bbc-news')
+#sources = getSources()
+#SNames = [source[0] for source in sources]
+newsSourceDict = getSourceBiasLists()
+print(newsSourceDict['leftLean'])
